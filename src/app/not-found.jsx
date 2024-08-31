d252609c1a24d944f4b9d4a6e638d9d69e0c9781
rayx81194 <mani.phartiyal@gmail.com>
2024-09-01 01:14:48 +0530

    not-found fix

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
-import Image from 'next/image'
- 
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function NotFound() {
   return (
     <div className='w-full h-full flex flex-col items-center mt-20'>
-        <Image src="/lost.png" width={200} height={200}></Image>
+      <Image src="/lost.png" width={200} height={200} alt="Lost page image" />
       <h2 className='font-bold mt-4 text-5xl'>Page does not exist</h2>
-      <p className='font-regular mt-3 text-3xl'>Hmm... are you sure you're at right place?</p>
+      <p className='font-regular mt-3 text-3xl'>Hmm. Are you sure you&apos;re at the right place?</p>
       <Link className='bg-red-500 px-5 mt-3 py-3 text-white shadow-md mx-auto rounded-[100px]' href="/">Return Home</Link>
     </div>
-  )
+  );
 }
